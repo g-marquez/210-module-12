e7f5f033372437b788804cb29bc819a70904bbf3
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-04-23 18:04:42 -0700

    Initial commit: starting lab 32

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//*****************************************************************************
+// COMSC-210 | Lab 32 | Gabriel Marquez
+// Description: this program simulate a line of cars lined up to pay their toll
+// at a toll booth, utilizing the std::deque container.
+//*****************************************************************************
+
+#include "Car.h"
+#include <iostream>
+#include <deque>
+using namespace std;
